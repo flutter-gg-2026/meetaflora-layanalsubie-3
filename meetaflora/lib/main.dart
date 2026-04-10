b8faa3139d4620cf0df8294f7b6c26c5b2a3e711
Layan Alsubaie <layanalsubie@gmail.com>
2026-04-10 22:03:13 +0300

    add cleany to project

--- meetaflora/lib/main.dart
+++ meetaflora/lib/main.dart
@@ -1,7 +1,26 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'core/setup.dart';
+import 'core/theme/app_theme.dart';
+import 'core/di/configure_dependencies.dart';
+import 'core/navigation/app_router.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EasyLocalization.ensureInitialized();
+  await setup();
+  await configureDependencies();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +28,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return Sizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp.router(
+          routerConfig: AppRouter.router,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          // themeMode: state.themeMode,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          debugShowCheckedModeBanner: true,
+        );
+      },
     );
   }
 }
+
